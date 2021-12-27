908b578e4eb825ec655fe9ea9a57e63cc45bd796
slee272 <slee272@student.hywoman.ac.kr>
2021-12-27 12:53:00 +0900

    git Test

--- app.js
+++ app.js
@@ -2,4 +2,28 @@ const title = document.querySelector(".hello:first-child h1");
 
 console.dir(title);
 
-title.innerText = "Hello";
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+const handel = () => {
+  title.style.color = "blue";
+};
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const sum = (a, b) => a + b;
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+title.onclick = handle;
+title.addEventListener("mouseenter", (a, b) => a + b);
+title.addEventListener("mouseleave", handleMouseLeave);
